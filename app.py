8a9aa184d95513bb50c3bf027b05618563259612
Peter Lee <pclee118@gmail.com>
2019-11-11 14:55:47 -0500

    Section 5

--- app.py
+++ app.py
@@ -3,61 +3,19 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required, current_identity
 
 from security import authenticate, identity
+from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
-
 app.secret_key = 'jose'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-items = []
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
-                        required=True,
-                        help="This field cannot be left blank!"
-    )
-
-    @jwt_required()
-    def get(self, name):
-        return {'item': next(filter(lambda x: x['name'] == name, items), None)}
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': "An item with name {} already exists.".format(name)}
-
-        data = Item.parser.parse_args()
-
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item
-
-    @jwt_required()
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
-
-    @jwt_required()
-    def put(self, name):
-        data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
